97c23beb4c80d1a5802414b87ae0635fa73b1901
rreyvido <reyvidod@outlook.com>
2023-01-11 08:30:37 +0700

    tailwind and router setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import ArticleList from "./pages/ArticleList";
+import Register from "./pages/Register";
+import ProductList from "./pages/ProductList";
+import NotFound from "./pages/NotFound";
+import Checkout from "./pages/Checkout";
+import Dashboard from "./adminPages/Dashboard";
+import Success from "./pages/Success";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/">
+          <Route index element={<Landing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="article">
+            <Route path="" element={<ArticleList />} />
+          </Route>
+          <Route path="product">
+            <Route path="" element={<ProductList />} />
+          </Route>
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="success" element={<Success />} />
+        </Route>
+        <Route path="/dashboard">
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
